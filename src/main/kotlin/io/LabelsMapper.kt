6e6d1caaeed000e01f6eaa7bbf9c2c825fc1d9ba
exaggeratedrumors <exaggeratedrumors@gmail.com>
2024-12-10 04:36:50 +0100

    models evaluation

--- src/main/kotlin/io/LabelsMapper.kt
+++ src/main/kotlin/io/LabelsMapper.kt
@@ -4,9 +4,9 @@ import com.ertools.common.Utils
 import java.io.File
 
 object LabelsMapper {
-    fun loadLabels(labels: List<Int>): List<Int> {
+    fun loadLabels(labels: List<Int>): List<Char> {
         val mappingFile = File(Utils.LABELS_BALANCED_DICTIONARY)
-        val mapping = mappingFile.readLines().map { it.toInt() }
+        val mapping = mappingFile.readLines().map { it.split(" ")[1].toInt().digitToChar() }
         return labels.map { mapping[it] }
     }
 }
